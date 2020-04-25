5f4526ad55d5d28679726722aa2b10ce58c964e3
Your Name <rikkitav@gmail.com>
2020-04-25 22:44:03 +0200

    design fixes

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,16 @@ class Home extends Component {
   render() {
     const { isAuthenticated, login } = this.props.auth;
     return (
+      <>
       <header>
         
         <img src={require("./img/header_img.png")} />
         <h1>Zarezerwuj salę</h1>
         <p>bez zbędnych telefonów</p>
       </header>
+
+      <slider><h2>Znajdź odpowiednią salę gimnastyczną</h2></slider>
+      </>
     );
   }
 }
