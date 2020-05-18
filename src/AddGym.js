daea77d1b03f397c3b2886a8b09b9d0bc58953e4
bsdrm <rikkitav@gmail.com>
2020-05-18 21:55:39 +0200

    add gym page hero

--- src/AddGym.js
+++ src/AddGym.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import Hero from "./Hero";
 import { useForm } from "react-hook-form";
 import Form from "./gymForm";
+import Banner from "./Banner";
 
 
 class AddGym extends Component {
     render(){
         return <>
-        <Hero></Hero>
+        <div id="pls"></div>
+        <Hero hero="gymsHero">
+            <Banner title="DODAJ SALĘ" banner="gymsBanner">
+                <div id="bar"></div>
+            </Banner>
+        </Hero>
         <Form></Form>
         </>;
 
--- src/Banner.js
+++ src/Banner.js
@@ -10,3 +10,4 @@ export default function Banner({children, title, subtitle}) {
         </div>
     )
 }
+
