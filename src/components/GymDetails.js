208af98618bc2fa434484bfe232b5e42839913ac
bsdrm <rikkitav@gmail.com>
2020-05-20 22:43:17 +0200

    added sample gym details

---
+++ src/components/GymDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Details = ({ details }) => {
+  return (
+    <div className="gym-details">
+      <p>{details}</p>
+      <div />
+    </div>
+  );
+};
+
+export default Details;
--- src/pages/GymProfile.js
+++ src/pages/GymProfile.js
@@ -3,6 +3,7 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from "react-router-dom";
 import GymName from '../components/GymName';
+import GymDetails from "../components/GymDetails";
 
 class Home extends Component {
     render() {
@@ -10,6 +11,9 @@ class Home extends Component {
       <div id="pls"></div>
       <div id="slash"></div>
       <GymName title="NAZWA OBIEKTU"></GymName>
+      <div id="constrain">
+      <GymDetails details="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla massa sapien, vehicula et lacus sit amet, rhoncus tincidunt sapien. Etiam vitae felis at nisi ultrices ultricies vitae non ipsum. Nunc pharetra nibh ac tellus vestibulum, quis mattis lacus ornare. Fusce euismod vel mauris ut porta. Praesent ullamcorper lacus quis felis hendrerit porttitor. Aenean consectetur turpis lacus. Phasellus eget velit imperdiet, facilisis neque tincidunt, sagittis ipsum. Nunc risus nulla, dictum ultrices neque eu, dapibus porttitor quam. Donec vestibulum dolor nec nibh condimentum interdum. Praesent interdum hendrerit auctor. Nullam ante tortor, molestie quis purus et, dictum convallis ante. Suspendisse eget risus tincidunt, imperdiet sem in, pellentesque elit. "></GymDetails>
+      </div>
       </>;
     }
   }
