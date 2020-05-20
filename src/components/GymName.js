b0b660b81da71420adde8a0a4a445f42045b7aa7
bsdrm <rikkitav@gmail.com>
2020-05-20 22:25:09 +0200

    added gym name

---
+++ src/components/GymName.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = ({ title }) => {
+  return (
+    <div className="gym-name">
+      <h4>{title}</h4>
+      <div />
+    </div>
+  );
+};
+
+export default Title;
--- src/pages/GymProfile.js
+++ src/pages/GymProfile.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from "react-router-dom";
+import GymName from '../components/GymName';
 
 class Home extends Component {
     render() {
       return <>
+      <div id="pls"></div>
+      <div id="slash"></div>
+      <GymName title="NAZWA OBIEKTU"></GymName>
       </>;
     }
   }
