d2ee5c7550235ad7ecf7bc6a8624b0dfde01423c
bsdrm <rikkitav@gmail.com>
2020-05-24 01:58:15 +0200

    form design fixes

--- src/pages/AddGym.js
+++ src/pages/AddGym.js
@@ -13,16 +13,22 @@ class AddGym extends Component {
     render(){
         return <>
         <div id="pls"></div>
+        <div id="slash"></div>
         <Hero hero="gymsHero">
             <Banner title="DODAJ SALĘ" banner="gymsBanner">
                 <div id="bar"></div>
             </Banner>
         </Hero>
-        <Title title="wypełnij dane"></Title>
-        <div id="container-1">
-            <Form></Form>
-            <div id="test-gallery">dodaj zdjęcie</div>
-            {/*<ImageUpload></ImageUpload>*/}
+        <div id="slash"></div>
+        
+        <div id="idk">
+        <div id="addGym_title">WYPEŁNIJ DANE</div>
+        <hr />
+            <div id="container-1">
+                <Form></Form>
+                <div id="test-gallery">dodaj zdjęcie</div>
+                {/*<ImageUpload></ImageUpload>*/}
+            </div>
         </div>
         </>;
 
