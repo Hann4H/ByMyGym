564e7aafbcf35b9afbe3d01da1ac222e0c47abf9
bsdrm <rikkitav@gmail.com>
2020-05-23 18:31:21 +0200

    added backend to form

--- src/components/gymForm.js
+++ src/components/gymForm.js
@@ -1,55 +1,82 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
+import firebase from "../firebase"
 
 
 export default function gymForm() {
     const {register, handleSubmit, errors} = useForm();
 
-    const onSubmit = (data) => {
-            console.log(data)
+    function onSubmit(e) {
+
+        firebase.firestore().collection('gyms').add({
+            gymName,
+            street,
+            city,
+            zip, 
+            height,
+            width,
+            length, 
+            audience,
+            changingRooms, 
+            price
+        })
+        .then(() => {
+            setGymName('')
+        })
     }
+
+    const [gymName, setGymName] = useState('')
+    const [street, setStreet] = useState('')
+    const [city, setCity] = useState('')
+    const [zip, setZip] = useState('')
+    const [height, setHeight] = useState('')
+    const [width, setWidth] = useState('')
+    const [length, setLength] = useState('')
+    const [audience, setAudience] = useState('')
+    const [changingRooms, setChangingRooms] = useState('')
+    const [price, setPrice] = useState('')
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="gymForm">
             <div className="container-2">
                 <label>Nazwa budynku</label>
-                <input type="text" name="gymName" ref={register} required/>
+                <input type="text" value = {gymName} name="gymName" onChange={e => setGymName(e.currentTarget.value)} ref={register} required/>
             </div>
             <div className="container-2">
                 <label>Ulica</label>
-                <input type="text" name="street" ref={register} required/>
+                <input type="text" value = {street} name="street" onChange={e => setStreet(e.currentTarget.value)} ref={register} required/>
             </div>
             <div className="container-2">
                 <label>Miasto</label>
-                <input type="text" name="city" ref={register} required/>
+                <input type="text" value ={city} name="city" onChange={e => setCity(e.currentTarget.value)} ref={register} required/>
             </div>
             <div className="container-2">
                 <label>Kod pocztowy</label>
-                <input type="text" name="zip" ref={register} required/>
+                <input type="text" value ={zip} name="zip" onChange={e => setZip(e.currentTarget.value)} ref={register} required/>
             </div>
             <div className="container-2">
                 <label>Wysokość</label>
-                <input type="text" name="height" min="1" ref={register} required/>
+                <input type="text" value ={height} name="height" onChange={e => setHeight(e.currentTarget.value)} min="1" ref={register} required/>
             </div>
             <div className="container-2">
                 <label>Szerokość</label>
-                <input type="text" name="width" min="1" ref={register} required/>
+                <input type="text" value ={width} name="width" onChange={e => setWidth(e.currentTarget.value)} min="1" ref={register} required/>
             </div>
             <div className="container-2">
                 <label>Długość</label>
-                <input type="text" name="length" min="1" ref={register} required/>
+                <input type="text" value ={length} name="length" onChange={e => setLength(e.currentTarget.value)} min="1" ref={register} required/>
             </div>
             <div className="container-2">
                 <label>Ilość miejsc na widowni</label>
-                <input type="text" name="audience" min="0" ref={register}/>
+                <input type="text" value ={audience} name="audience" onChange={e => setAudience(e.currentTarget.value)} min="0" ref={register}/>
             </div>
             <div className="container-2">
                 <label>Ilość szatń</label>
-                <input type="text" name="changingRooms" min="0" ref={register} required/>
+                <input type="text" value ={changingRooms} name="changingRooms" onChange={e => setChangingRooms(e.currentTarget.value)} min="0" ref={register} required/>
             </div>
             <div className="container-2">
                 <label>Cena za godzinę</label>
-                <input type="text" name="price" min="1" ref={register} required/>
+                <input type="text" value ={price} name="price" onChange={e => setPrice(e.currentTarget.value)} min="1" ref={register} required/>
             </div>
             <div></div><div></div>
             <button>DODAJ</button>
