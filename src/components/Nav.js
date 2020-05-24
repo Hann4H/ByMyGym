08d6355c38951be8e31e527cf05b538056a9cd5e
Hanna Hrytsko <hanna.hrytsko@gmail.com>
2020-05-24 15:22:08 +0200

    user profile link in navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,42 +1,59 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { FaAlignRight } from 'react-icons/fa';
-import { FaSearch } from 'react-icons/fa';
+import { FaAlignRight } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import { Route, Redirect, Switch } from "react-router-dom";
 
 class Nav extends Component {
-  state={
-    isOpen:false
-  }
-  handleToggle= () => {
-    this.setState({isOpen:!this.state.isOpen})
-  }
+  state = {
+    isOpen: false,
+    error: "",
+  };
+
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   render() {
     const { isAuthenticated, login, logout } = this.props.auth;
+    const user_name = localStorage.getItem("user_name");
     return (
       <nav>
         <ul>
           <li id="logo">
-            <Link to="/"><img src={require('../img/logo.png')} /></Link>
+            <Link to="/">
+              <img src={require("../img/logo.png")} alt="logo" />
+            </Link>
           </li>
-          {/*<li>
-            <Link to="/profile">Profile</Link>
-          </li>*/}
           <li>
-            <button className="dropDown" onClick={this.handleToggle}><FaAlignRight></FaAlignRight></button>
+            <button className="dropDown" onClick={this.handleToggle}>
+              <FaAlignRight></FaAlignRight>
+            </button>
           </li>
-          <ul> {/*className={this.state.isOpen?"nav-links show-nav":"nav-links"}>*/}
+          <ul>
+            {/*className={this.state.isOpen?"nav-links show-nav":"nav-links"}>*/}
             <li>
-              <button id="zalogujButton" onClick={isAuthenticated() ? logout : login}>
+              <button
+                id="zalogujButton"
+                onClick={isAuthenticated() ? logout : login}
+              >
                 {isAuthenticated() ? "WYLOGUJ" : "ZALOGUJ"}
               </button>
             </li>
-            <li>
-              <button id="dodajSaleButton"><Link to='/add'>DODAJ SALĘ</Link></button>
-              
+            <li style={isAuthenticated() ? {} : { display: "none" }}>
+              <Link class="user-profile-link" to="/profile">
+                {user_name}
+              </Link>
             </li>
             <li>
-              <button className="searchButton"><FaSearch size="43px"></FaSearch></button>
+              <button id="dodajSaleButton">
+                <Link to="/add">DODAJ SALĘ</Link>
+              </button>
+            </li>
+            <li>
+              <button className="searchButton">
+                <FaSearch size="43px"></FaSearch>
+              </button>
             </li>
           </ul>
         </ul>
