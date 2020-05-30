4d51017e6874f5a6c62d4992e6fc811a3e2492b5
bsdrm <rikkitav@gmail.com>
2020-05-30 21:06:55 +0200

    tried fixing api

--- src/JsonData.js
+++ src/JsonData.js
@@ -1,7 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, Component } from 'react';
 
 
 const JsonData = () => {
+
+    const initialState = {};
+
+    const [halls, setHalls] = useState([]);
+
+    const names = []; 
 
     useEffect( () => {
         getHalls();
@@ -10,13 +16,27 @@ const JsonData = () => {
     const getHalls = async () => {
         const response = await fetch(`https://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=class_objects&class_id=331&lang=pl&fbclid=IwAR2NxBA6WgC3kymw6uG8ZTfHPvOkAOfhn-d54FNCATm2yWt79JDTgJaKIq0`);
         const data = await response.json();
-        console.log(data);
+        const halls = data.features;
+        console.log(halls[0].properties.nazwa);
+        
+
     }
 
-    return (
-        <div></div>
-    )
 
-};
+
+
+        return (
+
+            <div>
+                {
+                halls.map(function(hall, i) {
+                    console.log('test')
+                    return <li key={i}>{hall}</li>
+                })
+                }
+            </div>
+        )
+   
+}
 
 export default JsonData;
