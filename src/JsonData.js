2287e96eda0bf762d8446268cbb32406d91ef965
Hanna Hrytsko <hanna.hrytsko@gmail.com>
2020-06-27 22:42:18 +0200

    open gym profile from main page

--- src/JsonData.js
+++ src/JsonData.js
@@ -1,58 +1,50 @@
-import React, { useEffect, useState, Component } from 'react';
-import { divIcon } from 'leaflet';
-import firebase from "./firebase"
-import JsonToFirebase from "./JsonToFirebase"
+import React, { useEffect, useState, Component } from "react";
+import { divIcon } from "leaflet";
+import firebase from "./firebase";
+import JsonToFirebase from "./JsonToFirebase";
 
 class JsonData extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
 
-        super(props);
+  componentDidMount() {
+    fetch(
+      `https://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=class_objects&class_id=331&lang=pl&fbclid=IwAR2NxBA6WgC3kymw6uG8ZTfHPvOkAOfhn-d54FNCATm2yWt79JDTgJaKIq0`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json.features,
+          isLoaded: true,
+        });
+        console.log(json.features);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-        this.state = {
-            items: [],
-            isLoaded: false
-        }
+  render() {
+    const { isLoaded, items } = this.state;
 
-    }
+    if (!isLoaded) return <div>Loading...</div>;
 
-    componentDidMount() {
-
-        fetch(`https://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=class_objects&class_id=331&lang=pl&fbclid=IwAR2NxBA6WgC3kymw6uG8ZTfHPvOkAOfhn-d54FNCATm2yWt79JDTgJaKIq0`)
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    items: json.features,
-                    isLoaded: true, 
-                })
-                console.log(json.features)
-            }).catch((err) => {
-                console.log(err);
-            });
-
-
-    }
-
-
-
-    render() {
-        const { isLoaded, items } = this.state;
-
-        if(!isLoaded)
-            return <div>Loading...</div>
-
-        return (
-            <>
-            {/*<JsonToFirebase json={items}></JsonToFirebase>*/}
-            <ul>
-                {items.map(item => (
-                    <li key={item.id}>
-                        {item.properties.nazwa}
-                    </li>
-                ))}
-            </ul>
-            </>
-        )
+    return (
+      <>
+        {/* <JsonToFirebase json={items}></JsonToFirebase> */}
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>{item.properties.nazwa}</li>
+          ))}
+        </ul>
+      </>
+    );
   }
 }
 
--- src/JsonToFirebase.js
+++ src/JsonToFirebase.js
@@ -1,22 +1,35 @@
-import React from 'react'
-import firebase from "./firebase"
+import React from "react";
+import firebase from "./firebase";
 
 function JsonToFirebase(props) {
+  const db = firebase.firestore();
 
-    const db = firebase.firestore();
+  const ref = db.collection("gyms").doc();
 
-    const ref = db.collection('gyms').doc()
+  props.json.map((item) =>
+    db.collection("gyms").add({
+      gymName: item.properties.nazwa,
+      street: item.properties.adres,
+      city: item.properties.miasto,
 
-    props.json.map(item => (
-        db.collection('gyms').add({
-            gymName: item.properties.nazwa,
-            street: item.properties.adres,
-            city: item.properties.miasto
-        })    
-    ))
+      id: item.id,
+      kod: item.properties.kod,
+      url: item.properties.url,
+      opis_klasy: item.properties.opis_klasy,
+      telefon: item.properties.telefon,
+      email: item.properties.email,
+      grafika: item.properties.grafika,
+      opis: item.properties.opis,
+      lat: item.geometry.coordinates[1],
+      lng: item.geometry.coordinates[0],
 
-    return null;
+      nazwa: item.properties.nazwa,
+      adres: item.properties.adres,
+      miasto: item.properties.miasto,
+    })
+  );
 
+  return null;
 }
 
 export default JsonToFirebase;
