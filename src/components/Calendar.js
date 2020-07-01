6f3871b7d3bb7686c6bf0de4c22d1e7200bceb0b
bsdrm <rikkitav@gmail.com>
2020-07-01 22:41:25 +0200

    started calendar >temporarily in home<

---
+++ src/components/Calendar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import moment from 'moment'
+import 'moment/locale/pl'
+
+moment.updateLocale('pl', { week : { dow : 2, doy : 4 } }); 
+
+
+export function Calendar() {
+
+    const weekdayshort = moment.weekdays();
+    
+    let weekdayshortname = weekdayshort.map(day => {
+        return (
+          <div className="weekday-name">
+            <th key={day} className="week-day">
+            {day}
+            </th>
+          </div>
+        );
+     });
+
+    return (
+        <div className="calendar">
+            <div className="calendar-day-name">
+                <tr>{weekdayshortname}</tr>
+            </div>
+        </div>
+    )
+
+}
