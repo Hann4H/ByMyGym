4a004df6eac988a6514a7b2540e7ef272e08612d
Hanna Hrytsko <hanna.hrytsko@gmail.com>
2020-07-02 11:38:37 +0200

    separate component for Gym Card in main page

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -4,6 +4,7 @@ import "firebase/firestore";
 import "firebase/storage";
 import ListingImg from "./ListingImg";
 import { Link, withRouter } from "react-router-dom";
+import GymItem from "../components/GymItem";
 
 const db = firebase.firestore();
 
@@ -14,31 +15,31 @@ class Listing extends Component {
     firebase
       .firestore()
       .collection("gyms")
-    //   .doc('OZiOSBKvQMcYsSPZIJuN')
-    //   .get()
-    //   .then(doc => {
-    //     const Gyms = [];
-    //       Gyms.push({
-    //         docId: doc.id,
-    //         gymName: doc.data().gymName,
-    //         street: doc.data().street,
-    //         zip: doc.data().zip,
-    //         city: doc.data().city,
-    //         height: doc.data().height,
-    //         width: doc.data().width,
-    //         length: doc.data().width,
-    //         price: doc.data().price,
-    //         id: doc.data().id,
-    //         photo: doc.data().photo
-        
-    //     });
-    //     this.setState({ Gyms });
-        
-    //   })
-    //   .catch(function(error) {
-    //     console.log("Error getting documents: ", error);
-    //   });
-    // }
+      //   .doc('OZiOSBKvQMcYsSPZIJuN')
+      //   .get()
+      //   .then(doc => {
+      //     const Gyms = [];
+      //       Gyms.push({
+      //         docId: doc.id,
+      //         gymName: doc.data().gymName,
+      //         street: doc.data().street,
+      //         zip: doc.data().zip,
+      //         city: doc.data().city,
+      //         height: doc.data().height,
+      //         width: doc.data().width,
+      //         length: doc.data().width,
+      //         price: doc.data().price,
+      //         id: doc.data().id,
+      //         photo: doc.data().photo
+
+      //     });
+      //     this.setState({ Gyms });
+
+      //   })
+      //   .catch(function(error) {
+      //     console.log("Error getting documents: ", error);
+      //   });
+      // }
       .orderBy("nazwa")
       .limit(10)
       .get()
@@ -92,46 +93,9 @@ class Listing extends Component {
   render() {
     return (
       <div>
-        {this.state.Gyms.map((gym, index) => {
-          return (
-            <div key={index} className="single-listing">
-              <div className="listing-content">
-                <div className="place-for-img">
-                  {/* <img id="myimg" src={gym.photo} /> */}
-                  {/* {this.image(gym.id) ? (
-                    <img id="myimg" src={this.image(gym.id)} alt="gym" />
-                  ) : (
-                    <div className="place-for-img">
-                      <img
-                        id="myimg"
-                        src={require("../img/no_image.svg.png")}
-                        alt="nothing"
-                      />
-                    </div>
-                  )} */}
-                </div>
-                <div className="gym-short-info">
-                  <h3 className="listing-header">{gym.gymName}</h3>
-                  <p>
-                    Adres: {gym.street}, {gym.city} {gym.zip}
-                  </p>
-                  <p>
-                    Wymiary: {gym.length}m x {gym.width}m x {gym.height}m
-                  </p>
-                  <p>Cena za godzinę: {gym.price}zł</p>
-
-                  <Link
-                    to={{
-                      pathname: `/gym_profile/${gym.docId}`,
-                    }}
-                  >
-                    <button>więcej informacji</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+        {this.state.Gyms.map((gym, index) => (
+          <GymItem key={gym.id} showCount={false} gym={gym} index={index} />
+        ))}
       </div>
     );
   }
