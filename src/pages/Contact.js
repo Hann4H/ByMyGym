f5dc4588a5f47bddbc9b0138094a2038c615b216
bsdrm <rikkitav@gmail.com>
2020-07-25 12:47:33 +0200

    contact front

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Form from 'react-bootstrap/Form';
 
 class Contact extends Component {
   render() {
@@ -10,18 +10,29 @@ class Contact extends Component {
 
         <div id="pls"></div>
 
-        <div id="idk">
-          <hr />
+        <div id="idk-8">
+
+          
           <div id="container-1">
-              <form>
-                  <p>
-                      <label>Imię</label>
-                      <input type="text" name="name"></input>
-                  </p>
-                  <p>
-                      <label>Wiadomość</label>
-                      <input type="text" name="message"></input>
-                  </p>
+              <form className="contact-form">
+
+                <p className="contact-p">Masz pytania? Chcesz uzyskać uprawnienia właściciela istniejącej już sali? <br />Napisz do nas!</p>
+                <hr />
+
+                <div>
+                  <label>Imię</label>
+                  <input className="name-input contact-input" />
+                </div>
+
+                <div class="message-contact">
+                  <label>Wiadomość</label>
+                  <textarea className="contact-input" rows="3"></textarea>
+                </div>
+
+                <button>
+                  WYŚLIJ
+                </button>
+
               </form>
           </div>
         </div>
