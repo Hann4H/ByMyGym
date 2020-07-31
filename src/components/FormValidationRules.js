688d5ea1db1c45ded2c778c02c3ca704e49887e9
bsdrm <rikkitav@gmail.com>
2020-08-01 00:11:38 +0200

    added validation regex

--- src/components/FormValidationRules.js
+++ src/components/FormValidationRules.js
@@ -6,10 +6,50 @@ export default function validate(values) {
       errors.gymName = 'Nazwa sali jest nieprawidłowa - minimum 3 znaki';
     }
     if (!values.gymStreet) {
-        errors.gymStreet = 'Należy wprowadzić ulicę';
-      } else if (!/(.*[a-z]){3}/.test(values.gymStreet)) {
-        errors.gymStreet = 'Nazwa ulicy jest nieprawidłowa - minimum 3 znaki';
-      }
+      errors.gymStreet = 'Należy wprowadzić ulicę';
+    } else if (!/(.*[a-z]){3}/.test(values.gymStreet)) {
+      errors.gymStreet = 'Nazwa ulicy jest nieprawidłowa - minimum 3 znaki';
+    }
+    if (!values.gymCity) {
+      errors.gymCity = 'Należy wprowadzić miasto';
+    } else if (!/(.*[a-z]){3}/.test(values.gymCity)) {
+      errors.gymCity = 'Nazwa miasta jest nieprawidłowa - minimum 3 znaki';
+    }
+    if (!values.gymZip) {
+      errors.gymZip = 'Należy wprowadzić kod pocztowy';
+    } else if (!/^\d{2}-\d{3}$/.test(values.gymZip)) {
+      errors.gymZip = 'Kod pocztowy jest nieprawidłowy';
+    }
+    if (!values.gymHeight) {
+      errors.gymHeight = 'Należy wprowadzić wysokość';
+    } else if (!/[0-9]+([\.,][0-9]+)?/.test(values.gymHeight)) {
+      errors.gymHeight = 'Wysokość jest nieprawidłowa';
+    }
+    if (!values.gymWidth) {
+      errors.gymWidth = 'Należy wprowadzić szerokość';
+    } else if (!/[0-9]+([\.,][0-9]+)?/.test(values.gymWidth)) {
+      errors.gymWidth = 'Szerokość jest nieprawidłowa';
+    }
+    if (!values.gymLength) {
+      errors.gymLength = 'Należy wprowadzić długość';
+    } else if (!/[0-9]+([\.,][0-9]+)?/.test(values.gymLength)) {
+      errors.gymLength = 'Długość jest nieprawidłowa';
+    }
+    if (!values.audience) {
+      errors.audience = 'Należy wprowadzić ilość miejsc na widowni';
+    } else if (!/[0-9]{1,3}/.test(values.audience)) {
+      errors.audience = 'Liczba jest nieprawidłowa';
+    }
+    if (!values.changingRooms) {
+      errors.changingRooms = 'Należy wprowadzić ilość szatń';
+    } else if (!/[0-9]{1,3}/.test(values.changingRooms)) {
+      errors.changingRooms = 'Liczba jest nieprawidłowa';
+    }
+    if (!values.gymPrice) {
+      errors.gymPrice = 'Należy wprowadzić cenę';
+    } else if (!/[0-9]{1,}/.test(values.gymPrice)) {
+      errors.gymPrice = 'Cena jest nieprawidłowa';
+    }      
     // console.log(errors);
     return errors;
   };
