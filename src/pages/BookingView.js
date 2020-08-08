8add34a4d41e23bb84304cbba8976d46a7c0c9de
Hanna Hrytsko <hanna.hrytsko@gmail.com>
2020-08-08 23:16:52 +0200

    add booking view page

--- src/pages/BookingView.js
+++ src/pages/BookingView.js
@@ -1,8 +1,57 @@
 import React, { Component } from "react";
+import firebase from "firebase";
+
+const db = firebase.firestore();
+
+function ListItems(props) {
+  return (
+    <>
+      <li>
+        <b>{props.gymName}</b>
+        <br />
+        {props.value}
+      </li>
+    </>
+  );
+}
 
 class BookingView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { bookingItems: [] };
+  }
+
+  componentDidMount() {
+    db.collection("gyms")
+      .orderBy("gymName")
+      .get()
+      .then((items) => {
+        const bookingItems = items.docs.map((doc) => {
+          return { docId: doc.id, ...doc.data() };
+        });
+        this.setState({ bookingItems: bookingItems });
+        this.bookingItems = bookingItems;
+        console.log(
+          "Show booking items: " + JSON.stringify(bookingItems, null, 4)
+        );
+      });
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div id="pls"></div>
+        <div className="admin-page">
+          {this.state.bookingItems.map((item) => (
+            <ListItems
+              key={JSON.stringify(this.state.bookingItems.docId)}
+              value={JSON.stringify(item, null, 4)}
+              gymName={JSON.stringify(item.gymName)}
+            />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
