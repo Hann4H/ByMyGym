c88dd1852f7103eeebd12394800c82bec44b6c1b
bsdrm <rikkitav@gmail.com>
2020-09-09 23:21:02 +0200

    worked on contact backend

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
-import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
 class Contact extends Component {
+
+  state = {
+    name: '',
+    message: '',
+    email: '',
+    sent: false,
+    buttonText: 'Send Message'
+  }
+
+  formSubmit = (e) => {
+    e.preventDefault()
+  
+    this.setState({
+        buttonText: '...sending'
+    })
+  
+    let data = {
+        name: this.state.name,
+        email: this.state.email,
+        message: this.state.message
+    }
+    
+    axios.post('API_URI', data)
+    .then( res => {
+        this.setState({ sent: true }, this.resetForm())
+    })
+    .catch( () => {
+      console.log('Message not sent')
+    })
+  }
+
+  resetForm = () => {
+    this.setState({
+        name: '',
+        message: '',
+        email: '',
+        buttonText: 'Message Sent'
+    })
+}
+
+
   render() {
     return (
       <>
