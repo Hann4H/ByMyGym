feb62ed6905dfd781a7cafaafa4abe9099bd8c37
bsdrm <rikkitav@gmail.com>
2020-09-21 09:00:30 +0200

    commit before pull

--- src/components/calendar/Basic.js
+++ src/components/calendar/Basic.js
@@ -17,6 +17,19 @@ import firebase from "firebase";
 
 const db = firebase.firestore();
 
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach((val) => val.length > 0 && (valid = false));
+  return valid;
+};
+
+const countErrors = (errors) => {
+  let count = 0;
+  Object.values(errors).forEach((val) => val.length > 0 && (count = count + 1));
+  return count;
+};
+
+
 class Basic extends Component {
   constructor(props) {
     super(props);
