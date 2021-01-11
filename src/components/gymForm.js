c47d046072d1cb1280de5539a7e65d2c65fb2548
Hanna Hrytsko <hanna.hrytsko@gmail.com>
2021-01-11 23:44:21 +0100

    fix drag and drop

--- src/components/gymForm.js
+++ src/components/gymForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import firebase from "../firebase";
 import "firebase/storage";
 import DragAndDrop from "./DragAndDrop";
@@ -10,21 +10,73 @@ import { add } from "date-fns";
 import { validateFields } from "../Validation";
 import Loading from "./Loading";
 
+import { useDropzone } from "react-dropzone";
+
 Modal.setAppElement("#root");
+
+// Drag and drop style
+const thumbsContainer = {
+	display: "flex",
+	flexDirection: "row",
+	flexWrap: "wrap",
+	marginTop: 16,
+};
+
+const thumb = {
+	display: "inline-flex",
+	borderRadius: 2,
+	border: "1px solid #eaeaea",
+	marginBottom: 8,
+	marginRight: 8,
+	width: 100,
+	height: 100,
+	padding: 4,
+	boxSizing: "border-box",
+};
+
+const thumbInner = {
+	display: "flex",
+	minWidth: 0,
+	overflow: "hidden",
+};
+
+const img = {
+	display: "block",
+	width: "auto",
+	height: "100%",
+};
+
+const styleContainer = {
+	flex: 1,
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	padding: "20px",
+	borderWidth: "2px",
+	borderRadius: "2px",
+	borderColor: "var(--darkOrange)",
+	borderStyle: "dashed",
+	backgroundColor: "#fafafa",
+	color: "#bdbdbd",
+	outline: "none",
+	transition: "border .24s ease-in-out",
+	cursor: "pointer",
+};
+// end Drag and drop style
 
 export default function gymForm() {
 	const [values, setValues] = useState({});
 	const [errors, setErrors] = useState({});
-	const [imageAsFile, setImageAsFile] = useState([]);
+	// const [imageAsFile, setImageAsFile] = useState([]);
 	const [userUID, setUserUID] = useState("");
 	const [text, setText] = useState("DODAJ");
 
-	const handleImageAsFile = (e) => {
-		for (let i = 0; i < e.target.files.length; i++) {
-			const image = e.target.files[i];
-			setImageAsFile((prevState) => [...prevState, image]);
-		}
-	};
+	// const handleImageAsFile = (e) => {
+	// 	for (let i = 0; i < e.target.files.length; i++) {
+	// 		const image = e.target.files[i];
+	// 		setImageAsFile((prevState) => [...prevState, image]);
+	// 	}
+	// };
 
 	firebase.auth().onAuthStateChanged(function (user) {
 		if (user) {
@@ -65,10 +117,10 @@ export default function gymForm() {
 				gymOwner: userUID,
 				accepted: false,
 
-				gymURL: values.gymURL || '',
+				gymURL: values.gymURL || "",
 				gymPhone: values.gymPhone,
 				gymEmail: values.gymEmail,
-				gymDescription: values.gymDescription || '',
+				gymDescription: values.gymDescription || "",
 				// gymLat: values.gymLat,
 				// gymLng: values.gymLng
 			}).then(() => {
@@ -118,6 +170,55 @@ export default function gymForm() {
 			[event.target.name]: event.target.value,
 		}));
 	};
+
+	console.log("imageAsFile", imageAsFile);
+
+	// drag and drop
+
+	const [imageAsFile, setFiles] = useState([]);
+	const { getRootProps, getInputProps } = useDropzone({
+		accept: "image/*",
+		onDrop: (acceptedFiles) => {
+			setFiles(
+				acceptedFiles.map((file) =>
+					Object.assign(file, {
+						preview: URL.createObjectURL(file),
+					})
+				)
+			);
+		},
+	});
+
+	const removeFile = (file) => () => {
+		const newFiles = [...imageAsFile];
+		newFiles.splice(newFiles.indexOf(file), 1);
+		setFiles(newFiles);
+	};
+
+	const thumbs = imageAsFile.map((file) => (
+		<div className="drag-drop-duo">
+			<div style={thumb} key={file.name}>
+				<div style={thumbInner}>
+					<img src={file.preview} style={img} alt="for upload" />
+				</div>
+			</div>
+			<button className="drag-drop-button" onClick={removeFile(file)}>
+				USUŃ
+			</button>
+		</div>
+	));
+
+	useEffect(
+		() => () => {
+			// Make sure to revoke the data uris to avoid memory leaks
+			imageAsFile.forEach((file) => URL.revokeObjectURL(file.preview));
+		},
+		[imageAsFile]
+	);
+
+	console.log("files 2", imageAsFile);
+
+	// end Drag and drop
 
 	return (
 		<form onSubmit={handleSubmit} className="gymForm" noValidate>
@@ -369,20 +470,38 @@ export default function gymForm() {
 						</div>
 					</div>
 				</div>
-				<div id="gallery" style={{ marginLeft: "20.5%" }}>
-					<input
+				<div id="gallery">
+					{/* style={{ marginLeft: "20.5%" }}  */}
+					<section className="container" style={styleContainer}>
+						<div {...getRootProps({ className: "dropzone" })}>
+							<input {...getInputProps()} />
+							<p>
+								Przeciągnij i upuść tutaj pliki lub kliknij, aby
+								wybrać
+							</p>
+						</div>
+						<aside style={thumbsContainer}>{thumbs}</aside>
+					</section>
+
+					{/* <input
 						type="file"
 						multiple="multiple"
 						id="img_url"
 						onChange={handleImageAsFile}
-					></input>
+					></input> */}
 					{errors.gymPhoto && (
 						<p className="help">{errors.gymPhoto}</p>
 					)}
 					{imageAsFile.length ? (
 						""
 					) : (
-						<p style={{ color: "#D83B0B", font: "10px Lato" }}>
+						<p
+							style={{
+								color: "#D83B0B",
+								font: "10px Lato",
+								marginTop: "0.5rem",
+							}}
+						>
 							Zdjęcie jest wymagane
 						</p>
 					)}
@@ -390,14 +509,24 @@ export default function gymForm() {
 
 				{/* TODO jeszcze z tym powalczę - z DragAndDrop */}
 				{/* <div id="gallery">
-          <DragAndDrop id="img_url" onChange={handleImageAsFile} />
-        </div> */}
+					<DragAndDrop id="img_url" onChange={handleImageAsFile} />
+				</div> */}
 			</div>
 			{/* <div className="gym-add-load">
 				{loading ? null : <Loading />}
 			</div> */}
-			<button  style={text === "DODAJ" ? { transform: "translate(280%, -200%)" } : { transform: "translate(160%, -200%)" }} className="form_button">{text}</button>       
+			<div style={{ textAlign: "center" }}>
+				<button
+					// style={
+					// 	text === "DODAJ"
+					// 		? { transform: "translate(280%, -200%)" }
+					// 		: { transform: "translate(160%, -200%)" }
+					// }
+					className="form_button"
+				>
+					{text}
+				</button>
+			</div>
 		</form>
-		
 	);
 }
