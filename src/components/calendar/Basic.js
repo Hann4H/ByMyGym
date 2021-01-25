51147edfd00f889c3667be3622793e8921ed5c38
ddubovskyy <ddubovskyy@gmail.com>
2021-01-26 00:14:03 +0100

    fix

--- src/components/calendar/Basic.js
+++ src/components/calendar/Basic.js
@@ -859,6 +859,7 @@ class Basic extends Component {
 						</div>
 						<p style={{ height: 10 }} />
 						{allFieldsValidated && (
+							<div>
 							<Tabs style={{ width: "60vw" }}>
 								<TabList>
 									<Tab>Rezerwacja krótkoterminowa</Tab>
@@ -944,6 +945,23 @@ class Basic extends Component {
 									</div>
 								</TabPanel>
 							</Tabs>
+							<p style={{ height: 10 }} />
+								<div className="range-picker-left mobile_pick">
+									<RangePickerForGym
+										name={this.state.name.value}
+										surname={this.state.surname.value}
+										email={this.state.email.value}
+										phoneNumber={
+											this.state.phoneNumber.value
+										}
+										user={this.state.user}
+										gym_id={this.props.gym_id}
+										ownerMail={this.state.ownerMail}
+										gymName={this.state.gymName}
+									/>
+								</div>
+								</div>
+							
 						)}
 					</form>
 
